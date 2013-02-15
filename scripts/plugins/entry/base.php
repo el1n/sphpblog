02917ff86950677c706e2c4649ef794c773760f5
ELIN <elin@mikomoe.jp>
2013-02-15 04:49:27 +0000

    logic update

--- scripts/plugins/entry/base.php
+++ scripts/plugins/entry/base.php
@@ -1,58 +1,122 @@
 <?php
 class base
 {
-	public $priority = 0x00;
+	public $priority = 0x01;
+	private $regx;
+
+	function base($s)
+	{
+		$this->regx = array
+		(
+			'/\[\[(?:(.+?):(?!\/{2}))?(?:([a-z]*?):(?!\/{2}))?(.+?)\]\]/iS' =>
+			function($m){
+				switch(true){
+					case preg_match('/s/',$m[2]):
+						$m[2] = '_self';
+						break;
+					case preg_match('/b/',$m[2]):
+					default:
+						$m[2] = '_blank';
+						break;
+				}
+				return(sprintf('<a href="%s" target="%s">%s</a>',$m[3],$m[2],$m[1] ? $m[1] : $m[3]));
+			},
+			'/\[\{(?:(.+?):(?!\/{2}))?(?:([a-z]*?):(?!\/{2}))?(.+?\.(?:bmp|gif|jpe?g|png))\}\]/iS' =>
+			function($m){
+				switch(true){
+					case preg_match('/p/',$m[2]):
+						$m[2] = ' popup=true';
+						break;
+					default:
+						$m[2] = ' popup=false';
+						break;
+				}
+				return(sprintf('[IMG=%s%s]',$m[3],$m[2]));
+			},
+			'/\[([!#$])?(\/?\w{2,})\]/isS' =>
+			function($m){
+				static $s = array
+				(
+					'BOLD' =>'<b>',
+					'/BOLD' =>'</b>',
+					'BLOCKQUOTE' =>'<blockquote>',
+					'/BLOCKQUOTE' =>'</blockquote>',
+					'CENTER' =>'<center>',
+					'/CENTER' =>'</center>',
+					'CODE' =>'<code>',
+					'/CODE' =>'</code>',
+					'DEL' =>'<del>',
+					'/DEL' =>'</del>',
+					'EM' =>'<em>',
+					'/EM' =>'</em>',
+					'H1' =>'<h1>',
+					'/H1' =>'</h1>',
+					'H2' =>'<h2>',
+					'/H2' =>'</h2>',
+					'H3' =>'<h3>',
+					'/H3' =>'</h3>',
+					'H4' =>'<h4>',
+					'/H4' =>'</h4>',
+					'H5' =>'<h5>',
+					'/H5' =>'</h5>',
+					'H6' =>'<h6>',
+					'/H6' =>'</h6>',
+					'HR' =>'<hr>',
+					'ITALIC' =>'<i>',
+					'/ITALIC' =>'</i>',
+					'INS' =>'<ins>',
+					'/INS' =>'</ins>',
+					'JS' =>'[HTML]<script type="text/javascript">',
+					'/JS' =>'</script>[/HTML]',
+					'CSS' =>'[HTML]<style type="text/css">',
+					'/CSS' =>'</style>[/HTML]',
+					'PRE' =>'<pre>',
+					'/PRE' =>'</pre>',
+					'STRIKE' =>'<strike>',
+					'/STRIKE' =>'</strike>',
+					'STRONG' =>'<strong>',
+					'/STRONG' =>'</strong>',
+					'UNDERLINE' =>'<underline>',
+					'/UNDERLINE' =>'</underline>',
+				);
+
+				switch($m[1]){
+					case '!':
+						return('['.$m[2].']');
+						break;
+					case '$':
+						return('<'.$m[2].'>');
+						break;
+					case '#':
+						return('&#'.$m[2].';');
+						break;
+					default:
+						break;
+				}
+				return(isset($s[strtoupper($m[2])]) ? $s[strtoupper($m[2])] : '['.$m[1].$m[2].']');
+			},
+			'/\[&lt;(.+?)(?::([a-z]*?))?&gt;\]/iS' =>
+			function($m){
+				switch(true){
+					case preg_match('/s/',$m[2]):
+						$m[2] = '_self';
+						break;
+					case preg_match('/b/',$m[2]):
+					default:
+						$m[2] = '_blank';
+						break;
+				}
+				return(sprintf('<a href="%s?entry=%s" target="%s">%s</a>',BASEURL,urlencode($m[1]),$m[2],$m[1]));
+			},
+		);
+	}
 
 	function main($s)
 	{
-		static $tags = array
-		(
-			'BOLD' =>'<b>',
-			'/BOLD' =>'</b>',
-			'BLOCKQUOTE' =>'<blockquote>',
-			'/BLOCKQUOTE' =>'</blockquote>',
-			'CENTER' =>'<center>',
-			'/CENTER' =>'</center>',
-			'CODE' =>'<code>',
-			'/CODE' =>'</code>',
-			'DEL' =>'<del>',
-			'/DEL' =>'</del>',
-			'EM' =>'<em>',
-			'/EM' =>'</em>',
-			'H1' =>'<h1>',
-			'/H1' =>'</h1>',
-			'H2' =>'<h2>',
-			'/H2' =>'</h2>',
-			'H3' =>'<h3>',
-			'/H3' =>'</h3>',
-			'H4' =>'<h4>',
-			'/H4' =>'</h4>',
-			'H5' =>'<h5>',
-			'/H5' =>'</h5>',
-			'H6' =>'<h6>',
-			'/H6' =>'</h6>',
-			'HR' =>'<hr>',
-			'ITALIC' =>'<i>',
-			'/ITALIC' =>'</i>',
-			'INS' =>'<ins>',
-			'/INS' =>'</ins>',
-			'JS' =>'[HTML]<script type="text/javascript">',
-			'/JS' =>'</script>[/HTML]',
-			'CSS' =>'[HTML]<style type="text/css">',
-			'/CSS' =>'</style>[/HTML]',
-			'PRE' =>'<pre>',
-			'/PRE' =>'</pre>',
-			'STRIKE' =>'<strike>',
-			'/STRIKE' =>'</strike>',
-			'STRONG' =>'<strong>',
-			'/STRONG' =>'</strong>',
-			'UNDERLINE' =>'<u>',
-			'/UNDERLINE' =>'</u>',
-		);
-
-		$s = preg_replace('/\[\[(?:(.+?):(?!\/{2}))?(.+?)\]\]/eiS','\'[URL=\2]\'.(strlen(\'\1\') ? \'\1\' : \'\2\').\'[/URL]\'',$s);
-		$s = preg_replace('/\[\{(?:(.+?):(?!\/{2}))?(.+?)\}\]/iS','[IMG=\2 popup=false]',$s);
-		return(preg_replace('/\[(#|\!)?(\/?[0-9A-Z]{2,})\]/eisS','\'\1\' ? ("\1" == "#" ? "&#\2;" : "<\2>") : (isset($tags[strtoupper(\'\2\')]) ? $tags[strtoupper(\'\2\')] : \'[\1\2]\')',$s));
+		foreach($this->regx as $regx =>$func){
+			$s = preg_replace_callback($regx,$func,$s);
+		}
+		return($s);
 	}
 }
 ?>
